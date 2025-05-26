1b14090b06444c3d2f87085e3a89a9ac2f5d75fd
Zektroz <tranminhtam7355@gmail.com>
2025-05-27 00:53:08 +0700

    Update password requirements

--- IndieGameZone.Domain/RequestsAndResponses/Requests/Users/UserForCreationDto.cs
+++ IndieGameZone.Domain/RequestsAndResponses/Requests/Users/UserForCreationDto.cs
@@ -14,12 +14,12 @@ namespace IndieGameZone.Domain.RequestsAndResponses.Requests.Users
         [Required]
         public required string Email { get; init; }
         [Required]
-        [RegularExpression(@"^[A-Za-z][A-Za-z0-9]{5,}$", ErrorMessage = "Username must be at least 5 characters long, contain only letters and numbers and must start with a letter.")]
+        [RegularExpression(@"^[A-Za-z][A-Za-z0-9]{4,}$", ErrorMessage = "Username must be at least 5 characters long, contain only letters and numbers and must start with a letter.")]
         public required string UserName { get; init; }
         [Required]
         public required DateOnly Birthday { get; init; }
         [Required]
-        [RegularExpression(@"^[A-Za-z0-9]{5,}$", ErrorMessage = "Password must be at least 5 characters long and contain only letters and numbers.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d])[^\s]{12,}$", ErrorMessage = "Password must be at least 12 characters long and include uppercase, lowercase, number, and special character.")]
         public required string Password { get; init; }
         [Required]
         [Compare("Password")]
