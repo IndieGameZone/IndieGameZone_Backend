dc212e15a78306d490cec2337ecbf0c96671c48c
Zektroz <tranminhtam7355@gmail.com>
2025-08-01 18:15:46 +0700

    Add report type when returning report records

--- IndieGameZone.Domain/RequestsAndResponses/Responses/ReportReasons/ReportReasonForReturnDto.cs
+++ IndieGameZone.Domain/RequestsAndResponses/Responses/ReportReasons/ReportReasonForReturnDto.cs
@@ -1,8 +1,12 @@
-﻿namespace IndieGameZone.Domain.RequestsAndResponses.Responses.ReportReasons
+﻿using IndieGameZone.Domain.Constants;
+
+namespace IndieGameZone.Domain.RequestsAndResponses.Responses.ReportReasons
 {
 	public class ReportReasonForReturnDto
 	{
 		public Guid Id { get; init; }
 		public string Name { get; init; }
-	}
+        public ReportReasonTypeEnum Type { get; init; }
+
+    }
 }
